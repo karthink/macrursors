5d82705520849ce7048f5dade36746e0b18ffbec
Karthik Chikmagalur <karthikchikmagalur@gmail.com>
2023-02-16 19:24:11 -0800

    macrursors-select-expand: Add expand-region support
    * macrursors-select-expand.el: Expand or contract the secondary
    selection with `macrursors-select-expand' and
    `macrursors-select-contract'.

---
+++ macrursors-select-expand.el
@@ -0,0 +1,30 @@
+(require 'macrursors-select)
+(require 'expand-region)
+
+;;;###autoload
+(defun macrursors-select-expand (&optional arg)
+  (interactive "p")
+  (unless (featurep 'expand-region)
+    (user-error "`macrursors-select-expand' requires `expand-region'."))
+  (let ((search-start) (search-end))
+    (save-excursion
+      (when (macrursors--inside-secondary-selection)
+        (setq search-start (overlay-start mouse-secondary-overlay)
+              search-end (overlay-end mouse-secondary-overlay))
+        (secondary-selection-to-region))
+      (let ((expand-region-fast-keys-enabled))
+        (if (< arg 0)
+            (er/contract-region (- arg))
+          (er/expand-region arg)))
+      (macrursors-select--region))
+
+    (macrursors-select--filter-cursors)
+    (macrursors-select--expand-cursors search-start search-end)
+    (set-transient-map macrursors-select-map)))
+
+;;;###autoload
+(defun macrursors-select-contract (&optional arg)
+  (interactive "p")
+  (macrursors-select-expand (- arg)))
+
+(provide 'macrursors-select-expand)
